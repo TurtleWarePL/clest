52bbf0372c67fee7172d60de3df144f8f79eee6d
jack <jack>
2018-04-15 07:14:30 +0000

    bring back removed application-frame

--- interface/application-frame.lisp
+++ interface/application-frame.lisp
@@ -10,6 +10,14 @@
       (format pane "Foobar 1.~%"))
     (format pane "No display method defined.~%")))
 
+(clim:define-application-frame clest (clim:standard-application-frame
+                                      project-parent-mixin)
+  ((tree-type :initarg :tree))
+  (:geometry :width 800 :height 1600)
+  (:pane (clim:make-pane (slot-value clim:*application-frame* 'tree-type)
+                         :root clim:*application-frame*))
+  (:menu-bar nil))
+
 (defun run-ui (&optional (tree 'simplistic-tree-gadget))
   (setf *projects* (clim:make-application-frame 'clest :tree tree))
   (clest/unit-tests::populate-data 'basic-project
